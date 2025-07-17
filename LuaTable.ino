e1386b4fd472a79f4d23376774dc90f1f39a7e61
KacperLa <42217156+KacperLa@users.noreply.github.com>
2025-07-17 18:50:46 -0400

    Add Lua integration example

--- LuaTable.ino
+++ LuaTable.ino
@@ -1,9 +1,51 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <Arduino.h>
+extern "C" {
+#include <lua.h>
+#include <lauxlib.h>
+#include <lualib.h>
+}
 
+// Lua interpreter state
+static lua_State* L;
+
+// C function exposed to Lua to toggle the on-board LED
+static int lua_toggle_led(lua_State* Lstate) {
+  static bool state = false;
+  state = !state;
+  digitalWrite(LED_BUILTIN, state ? HIGH : LOW);
+  return 0; // no return values
+}
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(LED_BUILTIN, OUTPUT);
+
+  // Initialize Lua
+  L = luaL_newstate();
+  luaL_openlibs(L);
+  // Register the LED toggle function
+  lua_register(L, "toggle_led", lua_toggle_led);
+
+  // Define a Lua function that will be called each loop
+  const char* script = "function loop() toggle_led() end";
+  if (luaL_dostring(L, script) != LUA_OK) {
+    Serial.print("Lua init error: ");
+    Serial.println(lua_tostring(L, -1));
+    lua_pop(L, 1);
+  }
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  Serial.println("Arduino main loop running");
 
+  // Call the Lua loop() function
+  lua_getglobal(L, "loop");
+  if (lua_pcall(L, 0, 0, 0) != LUA_OK) {
+    Serial.print("Lua runtime error: ");
+    Serial.println(lua_tostring(L, -1));
+    lua_pop(L, 1);
+  }
+
+  delay(1000); // Wait a bit between iterations
 }
+
